f784dde27e9823adcd9dfea009b140da6c4da5d7
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-05-12 12:47:21 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args)
   {
-    RWOnly rw=new RWOnly();
+    RWOnly rw=new RWOnly("Elangovan");
     rw.setter("Hi");
     System.out.println(rw.getter());
     // completed
